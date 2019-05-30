b686b2368ab3db000cf2eee7f94aee5b8ee94f9c
SvenOu <948077475@qq.com>
2019-05-30 17:55:56 +0800

    启动优化

--- lib_common/src/main/java/com/sv/common/CommonApplication.java
+++ lib_common/src/main/java/com/sv/common/CommonApplication.java
@@ -2,18 +2,13 @@ package com.sv.common;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatDelegate;
 
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.sv.common.executor.ScheduledExecutorManager;
+import com.sv.common.init.InitializeIntentService;
 import com.sv.common.util.Logger;
-import com.sv.lib_theme.ThemeManager;
-import com.umeng.analytics.MobclickAgent;
 
 public abstract class CommonApplication extends Application {
     private static final String TAG = CommonApplication.class.getSimpleName();
@@ -33,54 +28,11 @@ public abstract class CommonApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        initImageLoader();
-        initArouter();
-        initStyles();
-        initUmeng();
-        initScheduledExecutor();
+        startService(new Intent(this,
+                InitializeIntentService.class));
     }
 
-    private void initScheduledExecutor(){
-        if(null == scheduledExecutorManager){
-            scheduledExecutorManager = ScheduledExecutorManager.getInstance();
-            scheduledExecutorManager.start();
-        }
-    };
-
-    private void initStyles() {
-        ThemeManager.getInstance().init(this);
-    }
-
-    private void initUmeng() {
-        MobclickAgent.setDebugMode(isDebugMode());
-    }
-
-    private void initArouter() {
-        if (isDebugMode()) {
-            ARouter.openLog();
-            ARouter.openDebug();
-        }
-        ARouter.init(this);
-    }
-
-    private void initImageLoader() {
-        int DISK_CACHE_SIZE = 50 * 1024 * 1024;// 50 Mb
-        ImageLoaderConfiguration.Builder builder = new ImageLoaderConfiguration.Builder(
-                this).threadPriority(Thread.NORM_PRIORITY - 2)
-                .denyCacheImageMultipleSizesInMemory()
-                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
-                .diskCacheSize(DISK_CACHE_SIZE)
-                .tasksProcessingOrder(QueueProcessingType.LIFO);
-
-        if (isDebugMode()) {
-            builder.writeDebugLogs(); // Remove for release app
-        }
-        ImageLoaderConfiguration config = builder.build();
-        ImageLoader loader = ImageLoader.getInstance();
-        loader.init(config);
-    }
-
-    protected abstract boolean isDebugMode();
+    public abstract boolean isDebugMode();
 
     public static CommonApplication getInstance() {
         if (null == instance) {
@@ -88,5 +40,13 @@ public abstract class CommonApplication extends Application {
             return null;
         }
         return instance;
+    }
+
+    public ScheduledExecutorManager getScheduledExecutorManager() {
+        return scheduledExecutorManager;
+    }
+
+    public void setScheduledExecutorManager(ScheduledExecutorManager scheduledExecutorManager) {
+        this.scheduledExecutorManager = scheduledExecutorManager;
     }
 }
